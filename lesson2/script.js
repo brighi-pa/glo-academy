ce38722588da6a8a914f0e31330d4e8c9124c52a
Paulina Brighi <brighi.pa@gmail.com>
2019-10-21 15:43:06 +0300

    усложненное задание 2 урок

--- lesson2/script.js
+++ lesson2/script.js
@@ -21,3 +21,21 @@ console.log(addExpenses.split(','));
 
 budgetDay = money / 30;
 console.log(budgetDay);
+
+
+
+console.log('Усложненное задание урок 2');
+
+const num = '266219';
+const num1 = num[0];
+const num2 = num[1];
+const num3 = num[2];
+const num4 = num[3];
+const num5 = num[4];
+const num6 = num[5];
+
+console.log(num);
+multiplication = num1 * num2 * num3 * num4 * num5 * num6;
+let multiplication3 = multiplication **3;
+const multiplication4 = String(multiplication3);
+console.log(multiplication4.substr(0.2));
